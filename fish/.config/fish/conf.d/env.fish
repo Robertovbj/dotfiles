16ecf4b0e5a2ec7f2f9ca0ae2cceaeeb25c9bc13
Roberto Barbosa <roberto.rvbj@gmail.com>
2026-05-11 09:42:40 -0300

    Add local bin directory to PATH in env.fish

--- fish/.config/fish/conf.d/env.fish
+++ fish/.config/fish/conf.d/env.fish
@@ -9,6 +9,8 @@ source ~/.env
 # SSH Agent - fixed socket via systemd user service
 set -gx SSH_AUTH_SOCK /run/user/1000/ssh-agent.socket
 
+set PATH $PATH /home/administrador/.local/bin
+
 if status is-interactive
     ssh-add -l &>/dev/null; or ssh-add ~/.ssh/id_ed25519
 end

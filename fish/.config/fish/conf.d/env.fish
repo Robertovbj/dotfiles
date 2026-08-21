312cf4fb979d0be3bbd9149d1825c30ef960d013
Roberto Venancio Barbosa Junior <roberto.rvbj@gmail.com>
2026-08-21 16:30:25 -0300

    Corrige ssh

--- fish/.config/fish/conf.d/env.fish
+++ fish/.config/fish/conf.d/env.fish
@@ -6,12 +6,14 @@ function shenv
 end
 source ~/.env
 
-# SSH Agent - fixed socket via systemd user service
-set -gx SSH_AUTH_SOCK /run/user/1000/ssh-agent.socket
-
-set PATH $PATH /home/administrador/.local/bin
+fish_add_path ~/.local/bin
 fish_add_path ~/bin
 
-if status is-interactive
-    ssh-add -l &>/dev/null; or ssh-add ~/.ssh/id_ed25519
+if test -f /run/user/1000/ssh-agent.socket
+    # SSH Agent - fixed socket via systemd user service
+    set -gx SSH_AUTH_SOCK /run/user/1000/ssh-agent.socket
+
+    if status is-interactive
+        ssh-add -l &>/dev/null; or ssh-add ~/.ssh/id_ed25519
+    end
 end

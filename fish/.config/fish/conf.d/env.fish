6c9a121e19ecd35d8892b7b96f9b11aa01b8331f
Roberto Barbosa <roberto.rvbj@gmail.com>
2026-08-25 15:13:32 -0300

    Remove hypr conf files

--- fish/.config/fish/conf.d/env.fish
+++ fish/.config/fish/conf.d/env.fish
@@ -9,7 +9,7 @@ source ~/.env
 fish_add_path ~/.local/bin
 fish_add_path ~/bin
 
-if test -f /run/user/1000/ssh-agent.socket
+if test -S /run/user/1000/ssh-agent.socket
     # SSH Agent - fixed socket via systemd user service
     set -gx SSH_AUTH_SOCK /run/user/1000/ssh-agent.socket
 
